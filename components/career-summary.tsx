c0d7bf96425a94b324a4fa6ab3be25e8ead22f0c
David <dcjohnson85@gmail.com>
2024-03-25 20:41:07 +0000

    Added grammerly fixes

--- components/career-summary.tsx
+++ components/career-summary.tsx
@@ -5,7 +5,8 @@ export default function CareerSummary() {
                 <h2>Summary</h2>
             </div>
             <div className="pl-5">
-                I am an engineering leader who has worked in the technology industry for 15 years. Over the last 5 years I have moved into a leadership role, from a hands on technical lead, with line management responsibilities, to more recently an engineering manager, managing multiple teams. In addition to my management skills, I also come from a full stack software engineering background, and have contributed either directly or by leadership of my engineering teams on large scale projects.
+                I am an engineering leader who has worked in the technology industry for 15 years. Over the last 5 years, I have moved into a leadership role, from a hands-on technical lead, with line management responsibilities, to 
+                more recently an engineering manager, managing multiple teams. In addition to my management skills, I also come from a full-stack software engineering background and have contributed either directly or by leadership of my engineering teams on large-scale projects.
             </div>
         </div>
     </>
--- data/jobs/heliosx.ts
+++ data/jobs/heliosx.ts
@@ -13,12 +13,12 @@ export const HeliosXJob: IJob = {
         objectives: ["Take over running of the engineering team for flagship products.",
             "Assess the technical skills within the team.",
             "Review priorities and roadmap for product development.",
-            "Improve SCRUM processes and implement correct Agile working practises.",
+            "Improve SCRUM processes and implement correct Agile working practices.",
             "Ensure delivery of commitments by the engineering team."],
-        achievements: ["Implemented knowledge transfer of SCRUM, agile wasy of working, estimation and velocity tracking, resulting in consistent delivery of commitments week on week. This also increased the frequency of deliverables, and improving the internal reputation of the engineering team.",
-            "As intermin product owner, I re-prioritised items on the product roadmap resulting in delivery of revenue generating initiatives sooner that boosted revenue by ~$12,000 per month.",
+        achievements: ["Implemented knowledge transfer of SCRUM, agile ways of working, estimation and velocity tracking, resulting in consistent delivery of commitments week on week. This also increased the frequency of deliverables and improved the internal reputation of the engineering team.",
+            "As intermin product owner, I re-prioritised items on the product roadmap resulting in the delivery of revenue-generating initiatives sooner that boosted revenue by ~$12,000 per month.",
             "Instrumental in defining the microservice architecture platform that the technology was to migrate to, resulting in a scaleable architecture for the company to expand its user base."],
-        reasonForLeaving: ["The companies financial state meant redundancies shortly after I joined. I was looking for a company with stability at this point."],
+        reasonForLeaving: ["The company's financial state meant redundancies shortly after I joined. I was looking for a company with stability at this point."],
         skills: ['Leadership', 'Line Management', 'Product Management', 'Strategic Planning', 'SCRUM / Agile', 'Performance Management', 'Stakeholder Management', "Capacity Planning", 'Team Goal / OKR Tracking']
     }
 }
