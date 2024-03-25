0900995495b620124c46f80270283bfa559afbc3
David <dcjohnson85@gmail.com>
2024-03-25 16:59:23 +0000

    Added results to achievements

--- data/jobs/heliosx.ts
+++ data/jobs/heliosx.ts
@@ -15,11 +15,9 @@ export const HeliosXJob: IJob = {
             "Review priorities and roadmap for product development",
             "Improve scrum processes and implement correct Agile working practises",
             "Get team to a point where they are hitting their commitments"],
-        achievements: ["Implemented knowledge transfer of SCRUM, agile, estimation and velocity tracking, resulting in consistent delivery of commitments week on week",
-            "Worked as intrim Product Owner after the previous PO was made redundant",
-            "Re-prioritised items on the product roadmap resulting in delivery of revenue generating initiatives that boosted revenue by ~$12,000 per month, and improved the internal reputation of the engineering team",
-            "Refine scrum process and provided training to ensure that all ceremonies were being performed in a way ensuring everybody is engaged",
-            "Instrumental in defining the microservice architecture platform that the technology was to migrate to"],
+        achievements: ["Implemented knowledge transfer of SCRUM, agile, estimation and velocity tracking, resulting in consistent delivery of commitments week on week, improving the frequency of deliverables, setting correct expectations and improving the internal reputation of the engineering team",
+            "As intermin product owner, I re-prioritised items on the product roadmap resulting in delivery of revenue generating initiatives that boosted revenue by ~$12,000 per month",
+            "Instrumental in defining the microservice architecture platform that the technology was to migrate to, resulting in a scaleable architecture for the company to expand its user base"],
         reasonForLeaving: ["The companies financial state meant redundancies shortly after I joined. I was looking for a company with stability at this point"],
         skills: ['Leadership', 'Line Management', 'Product Management', 'Strategic Planning', 'SCRUM / Agile', 'Performance Management', 'Stakeholder Management', "Capacity Planning", 'Team Goal / OKR Tracking']
     }
