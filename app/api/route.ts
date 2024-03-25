f06958dbea331567abc28f841b9fb471f4003084
David <dcjohnson85@gmail.com>
2024-03-25 10:59:15 +0000

    Added initial framework for CV PDF endpoint

---
+++ app/api/route.ts
@@ -0,0 +1,9 @@
+// app/api/route.js 👈🏽
+
+import { NextResponse } from "next/server";
+
+// To handle a GET request to /api
+export async function GET() {
+  // Do whatever you want
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
